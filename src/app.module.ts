5fac617ae24b67b70b7cf8704f631269d393575d
alexandre-dev29 <axel.business29@gmail.com>
2022-12-13 11:28:06 +0200

    updating dependencies and configure Graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { YogaDriver, YogaDriverConfig } from '@graphql-yoga/nestjs';
+import { GraphQLLiveDirective, useLiveQuery } from '@envelop/live-query';
+import { useResponseCache } from '@graphql-yoga/plugin-response-cache';
+import { useGraphQlJit } from '@envelop/graphql-jit';
+import { createRedisCache } from '@envelop/response-cache-redis';
+import Redis from 'ioredis';
+import { GetInMemoryStore } from '../libs/utility';
+import { UsersModule } from './users/users.module';
+
+const redis = new Redis({});
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<YogaDriverConfig>({
+      driver: YogaDriver,
+      autoSchemaFile: true,
+      graphiql: true,
+      installSubscriptionHandlers: true,
+      logging: true,
+      buildSchemaOptions: {
+        directives: [GraphQLLiveDirective],
+      },
+      plugins: [
+        useResponseCache({
+          session: () => null,
+          ttl: 10000 * 6,
+          cache: createRedisCache({ redis }),
+        }),
+        useGraphQlJit(),
+        useLiveQuery({ liveQueryStore: GetInMemoryStore() }),
+      ],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyMultipart from '@fastify/multipart';
+import * as fastifyCookie from '@fastify/cookie';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const port = process.env.PORT || 3333;
+  await app.register(fastifyMultipart);
+  await app.register(fastifyCookie);
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 bootstrap();
